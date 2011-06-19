9923fd5dfd91b6fe4c9a43b41bd28e2127686fac
FUKAUMI Naoki <naobsd@gmail.com>
2011-06-19 12:15:08 +0900

    disable some extra packages for smaller system

--- cyanogen_tcc8902.mk
+++ cyanogen_tcc8902.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/telechips/tcc8902/full_tcc8902.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+$(call inherit-product, vendor/cyanogen/products/common.mk)
 
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
