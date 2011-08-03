f9f3107b993eef83ae57c61b0816a1a50fac7ba3
FUKAUMI Naoki <naobsd@gmail.com>
2011-08-03 19:07:45 +0900

    remove support for Gpad701, M1001, and M1001 8K. they are supported in tcc8902gb repo.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_d10_8k.mk \
     $(LOCAL_DIR)/cyanogen_em51.mk \
     $(LOCAL_DIR)/cyanogen_em71.mk \
-    $(LOCAL_DIR)/cyanogen_gpad701.mk \
-    $(LOCAL_DIR)/cyanogen_m1001.mk \
-    $(LOCAL_DIR)/cyanogen_m1001_8k.mk \
     $(LOCAL_DIR)/cyanogen_p7.mk \
     $(LOCAL_DIR)/cyanogen_p7_8k.mk \
     $(LOCAL_DIR)/cyanogen_scroll7.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,9 +4,6 @@ add_lunch_combo cyanogen_cherry-eng
 add_lunch_combo cyanogen_d10_8k-eng
 add_lunch_combo cyanogen_em51-eng
 add_lunch_combo cyanogen_em71-eng
-add_lunch_combo cyanogen_gpad701-eng
-add_lunch_combo cyanogen_m1001-eng
-add_lunch_combo cyanogen_m1001_8k-eng
 add_lunch_combo cyanogen_p7-eng
 add_lunch_combo cyanogen_p7_8k-eng
 add_lunch_combo cyanogen_scroll7-eng
