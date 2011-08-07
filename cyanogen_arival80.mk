b38a50042128f44c6a090548f5b347a1dd83f564
FUKAUMI Naoki <naobsd@gmail.com>
2011-08-07 10:41:43 +0900

    fix accelerometer

--- cyanogen_arival80.mk
+++ cyanogen_arival80.mk
@@ -26,7 +26,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/telechips/tcc8902/overlay
 # Accelerometer hacks
 PRODUCT_PROPERTY_OVERRIDES += \
     sensor.accelerometer.flip_xy=1 \
-    sensor.accelerometer.invert_x=1
+    sensor.accelerometer.invert_y=1
 
 #
 # Set ro.modversion
